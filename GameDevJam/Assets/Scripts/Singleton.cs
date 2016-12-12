adfd602b7ebeab5f59a9d2d8b54e49c0c9b2f943
Matthew Russo <mcr431@nyu.edu>
2016-12-12 10:08:42 -0500

    Added some standard scripts we can adapt

---
+++ GameDevJam/Assets/Scripts/Singleton.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace Patterns {
+    public class Singleton<T> : MonoBehaviour where T : Component {
+        private static T _instance;
+
+        public static T Instance {
+            get {
+                //Check
+                if (_instance == null) {
+                    //Checks to see if there is already an instantiation of specific singleton.
+                    var objs = FindObjectsOfType(typeof(T)) as T[];
+                    if (objs.Length > 0)
+                        _instance = objs[0];
+                    if (objs.Length > 1)
+                    {
+                        Debug.Log("There is more than one " + typeof(T).Name + " in the scene.");
+                    }
+                    //Creates instance if none exists.
+                    if (_instance == null)
+                    {
+                        GameObject obj = new GameObject();
+                        obj.hideFlags = HideFlags.HideAndDontSave;
+                        _instance = obj.AddComponent<T>();
+                    }
+                }
+
+                return _instance;
+            }
+        }
+    }
+
+    /// <summary>
+    /// USE WHEN SINGLETON NEEDS TO PERSIST THROUGH LEVEL LOADS
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class SingletonPersistent<T> : MonoBehaviour
+        where T : Component
+    {
+        public static T Instance { get; private set; }
+
+        public virtual void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this as T;
+                DontDestroyOnLoad(this);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ GameDevJam/Assets/Scripts/TiledBackground.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace PrototypingKit{
+
+    /// <summary>
+    /// Scales and repeats background textures regardless of screen size.
+    /// REQUIRES PIXELPERFECTCAMERA SCRIPT TO WORK
+    /// </summary>
+    public class TiledBackground : MonoBehaviour{
+
+        public int textureSize = 32;
+        public bool scaleHorizontally = true;
+        public bool scaleVertically = true;
+
+
+        void Start(){
+            var newWidth = !scaleHorizontally ? 1 : Mathf.Ceil(Screen.width / (textureSize * PixelPerfectCamera.scale));
+            var newHeight = !scaleVertically ? 1 : Mathf.Ceil(Screen.height / (textureSize * PixelPerfectCamera.scale));
+
+            transform.localScale = new Vector3(newWidth * textureSize, newHeight * textureSize, 1);
+
+            GetComponent<Renderer>().material.mainTextureScale = new Vector3(newWidth, newHeight, 1);
+        }
+    }
+}
