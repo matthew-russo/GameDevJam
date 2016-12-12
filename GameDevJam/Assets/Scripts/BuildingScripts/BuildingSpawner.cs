c651b80be2b77ea460545d79a7d08e05a982d36f
Matthew Russo <mcr431@nyu.edu>
2016-12-12 11:04:37 -0500

    Added building spawner & destroyer

---
+++ GameDevJam/Assets/Scripts/BuildingScripts/BuildingSpawner.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildingSpawner : MonoBehaviour {
+
+    public GameObject prefab;
+    private Vector3 spawnPos;
+    private Quaternion spawnRot;
+    private float spawnTime;
+
+	// Use this for initialization
+	void Start () {
+        spawnTime = 2f;
+        spawnPos = transform.position;
+        spawnRot = transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        spawnTime -= Time.deltaTime;
+        if (spawnTime < 0)
+        {
+            spawnTime = 2f;
+            Instantiate(prefab, spawnPos, spawnRot);
+        }
+	}
+}
---
+++ GameDevJam/Assets/Scripts/BuildingScripts/MoveBuildings.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBuildings : MonoBehaviour {
+
+    private Vector3 movement;
+
+	// Use this for initialization
+	void Start () {
+        movement = new Vector3(-.05f, 0f, 0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(movement);
+	}
+}
