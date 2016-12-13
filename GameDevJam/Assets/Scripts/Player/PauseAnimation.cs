739645066524dacf2fc31c7ee4b5f6b32ea273be
Matthew Russo <mcr431@nyu.edu>
2016-12-13 12:03:25 -0500

    Added Sound, Fixed bugs with Pause

---
+++ GameDevJam/Assets/Scripts/Player/PauseAnimation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseAnimation : MonoBehaviour
+{
+
+    private Animator _animator;
+
+    // Use this for initialization
+    private void Start ()
+	{
+	    _animator = GetComponent<Animator>();
+	}
+
+    // Update is called once per frame
+    private void Update () {
+        if (GlobalPause.Instance.isPaused)
+        {
+            _animator.speed = 0;
+        }
+        else
+        {
+            _animator.speed = 1;
+        }
+	}
+}
