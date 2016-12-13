3e67f6cbd55d6a387d4258537847bf23d6ab79d8
Tarik <tz638@nyu.edu>
2016-12-13 11:38:27 -0500

    Added HighScore table and scene

--- GameDevJam/Assets/Scripts/Player/PlayerOffscreen.cs
+++ GameDevJam/Assets/Scripts/Player/PlayerOffscreen.cs
@@ -5,11 +5,18 @@ using UnityEngine.SceneManagement;
 public class PlayerOffscreen : MonoBehaviour {
 
     private bool offscreen;
+    private Distance distanceScript;
+    private string[] prefArray;
 
 	// Use this for initialization
 	void Start () {
-	
-	}
+
+        distanceScript = (Distance)FindObjectOfType(typeof(Distance));
+        prefArray = new string[5]
+        {
+            "HighScore5", "HighScore4", "HighScore3", "HighScore2", "HighScore"
+        };
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -25,7 +32,25 @@ public class PlayerOffscreen : MonoBehaviour {
 	}
 
     private void gameOver()
+    {        
+        calculateHS();
+        SceneManager.LoadScene("End");        
+    }
+
+    private void calculateHS()
     {
-        SceneManager.LoadScene("End");
+        var i = 0;
+
+        PlayerPrefs.SetFloat("LastPoints", Mathf.Round(distanceScript.distanceTotal));
+
+        while (i < 5 && PlayerPrefs.GetFloat("LastPoints") > PlayerPrefs.GetFloat(prefArray[i], 0))
+        {
+            if (i != 0) PlayerPrefs.SetFloat(prefArray[i - 1], PlayerPrefs.GetFloat(prefArray[i]));
+
+            PlayerPrefs.SetFloat(prefArray[i], PlayerPrefs.GetFloat("LastPoints"));
+            i++;
+        }
+
+        Debug.Log(PlayerPrefs.GetFloat("HighScore"));
     }
 }
---
+++ GameDevJam/Assets/Scripts/UI/ScoreDisplay.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreDisplay : MonoBehaviour {
+
+    public Text score;
+
+	// Use this for initialization
+	void Start () {
+
+        score.text = ("You got " + PlayerPrefs.GetFloat("LastPoints") + " points. Your highscore is " + PlayerPrefs.GetFloat("HighScore") + ".");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
